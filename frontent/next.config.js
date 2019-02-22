9d18d9626794807ead38a0866d66d052ee1b21ad
pmdownie <pmdownie@gmail.com>
2019-02-22 09:51:45 +0100

    Made production ready and deployed to production.

--- frontent/next.config.js
+++ frontent/next.config.js
@@ -2,12 +2,12 @@ const dotEnvResult = require('dotenv').config()
 const prod = process.env.NODE_ENV === 'production'
 
 if (dotEnvResult.error) {
-  throw dotEnvResult.error
+    throw dotEnvResult.error
 }
 
 module.exports = {
     target: 'serverless',
     env: {
-      ENDPOINT: prod ? 'https://nd-api.whilde.studio' : process.env.ENDPOINT
-    }
-  }
+        ENDPOINT: prod ? 'https://api.nadinedewart.com' : process.env.ENDPOINT,
+    },
+}
